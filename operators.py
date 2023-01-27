e62c209f70f6c5e3c0adce47caad1c5fb9425b2c
Aklima Bekdaulet <a_bekdaulet@kbtu.kz>
2023-01-27 14:51:23 +0600

    TSIS1

---
+++ operators.py
@@ -0,0 +1,15 @@
+print(10 + 5)
+
+""""
+
+Operator	Name	            Example	
++	        Addition	        x + y	
+-	        Subtraction	        x - y	
+*	        Multiplication	    x * y	
+/	        Division	        x / y	
+%	        Modulus	            x % y	
+**	        Exponentiation	    x ** y	
+//	        Floor division	    x // y	
+
+
+"""
